e5badd471d50399e1ea5c749a48e017faf16c770
masifafzal81 <masifafzal81@email.com>
2025-07-28 20:03:56 +0500

    Add Category Form

---
+++ sgfarm/webapp/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('logout/', views.logout_view, name='logout'),
+    path('add_category/', views.add_category, name='add_category'),
+    
+]
--- sgfarm/webapp/views.py
+++ sgfarm/webapp/views.py
@@ -1,3 +1,40 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
-# Create your views here.
+def home(request):
+    
+   
+    # check the logged in user
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        
+        # authenticate the user
+    
+        user = authenticate(request, username=username, password=password)
+    
+        if user is not None:
+            login(request, user)
+            messages.success(request, 'You are now logged in.')
+            return redirect('home')
+        
+        else:
+            messages.success(request, 'Invalid username or password')
+            return redirect('home')        
+    
+    else:
+        return render(request, 'home.html', {})
+
+    
+def logout_view(request):
+    logout(request)
+    messages.success(request, 'You are successfully logged out.')
+    return redirect('home')
+
+def add_category(request):
+    if request.user.is_authenticated:
+        return render(request, 'add_category.html',{})
+    else:
+        messages.success(request,'You have to login.....')
+        return redirect('home')
